8b9f4f683faedfb648c230dd4b4a42730564b95c
Danil Deineka <tripledpadik@gmail.com>
2023-03-02 22:17:27 +0200

    end tune lib

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,11 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 const galleryList = document.querySelector(".gallery");
-
-
 
 const markup = galleryItems
   .map(
@@ -22,6 +20,8 @@ const markup = galleryItems
 
 galleryList.insertAdjacentHTML("beforeend", markup);
 
-
-const lightbox = new SimpleLightbox('.gallery a');
-
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+  scrollZoom: false,
+});
