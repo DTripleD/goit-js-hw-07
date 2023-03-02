d8991f47f1f7e2ecb167d3fd166d7eda9d49680f
Danil Deineka <tripledpadik@gmail.com>
2023-03-02 14:08:20 +0200

    make markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}" onclick="event.preventDefault()">
+            <img
+            class="gallery__image" 
+            src="${item.preview}" 
+            alt="${item.description}" 
+            data-src="${item.original}">
+        </a>
+    </div>`
+  )
+  .join("");
+
+console.log(markup);
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+const smalImg = document.querySelectorAll(".gallery__image");
+
+galleryList.addEventListener("click", (event) => {
+  smalImg.forEach(() => {
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+    event.target.src = event.target.dataset.src;
+  });
+});
 
 
-const mainGallery = document.querySelector(".gallery");
+// document.querySelector('button.image').onclick = () => {
 
-const galleryItem = document.createElement("img");
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`).show()
+// }
 
-console.log(galleryItems);
 
-
-galleryItem.src = galleryItems.preview;
-galleryItem.alt = galleryItems.description;
-
-mainGallery.append(galleryItem.src);
+const open =  basicLightbox.create(`<img src="${event.target.dataset.src}" alt="" width="800" height="600">`);
+open.show();
