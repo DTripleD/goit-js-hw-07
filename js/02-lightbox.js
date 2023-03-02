eb05731fa47fd3f1004adc32724dd750b187455a
Danil Deineka <tripledpadik@gmail.com>
2023-03-02 19:28:41 +0200

    add SimpleLightbox lib

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+
+
+const markup = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+            data-src="${original}">
+        </a>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+
+const lightbox = new SimpleLightbox('.gallery a');
+
